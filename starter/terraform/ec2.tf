52ebca2de0c6a8b9d8ff19bd3cbcb9a2855bd163
Omar M. Fathy <OmarMFathy219@gmail.com>
2023-01-25 03:50:57 +0200

    Update ami image

--- starter/terraform/ec2.tf
+++ starter/terraform/ec2.tf
@@ -11,7 +11,7 @@
    source             = "./modules/ec2"
    name               = local.name
    account            = data.aws_caller_identity.current.account_id
-   aws_ami            = "ami-0ef8c7099ef697f3e"
+   aws_ami            = "ami-0406ac3c60cab34c3"
    private_subnet_ids = module.vpc.private_subnet_ids
    vpc_id             = module.vpc.vpc_id
  }
